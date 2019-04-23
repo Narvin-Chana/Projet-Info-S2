c4f582d9af63b01d23b6abc7623c4b8e9512b0a9
Camemba <48033942+Camemba@users.noreply.github.com>
2019-04-23 19:00:49 +0200

    Update article.php

--- article.php
+++ article.php
@@ -1,12 +1,14 @@
-﻿<?php
+<?php
 require("main.php");
 session_start();
-$article_id=$_SESSION["search"];
+
+//$article_id=$_GET["id"];
+$article_id=1;
 $req_img = "SELECT article.IMAGE FROM article where article.ID_ARTICLE={$article_id}; ";
 $req_nom_produit = "SELECT article.NOM_ARTICLE FROM article where article.ID_ARTICLE={$article_id}; ";
-$req_prix_ht = "SELECT article.PRIX_HT FROM article where article.ID_ARTICLE={$article_id}; ";
+//$req_prix_ht = "SELECT article.PRIX_HT FROM article where article.ID_ARTICLE={$article_id}; ";
 $req_prix_ttc = "SELECT article.PRIX_TTC FROM article where article.ID_ARTICLE={$article_id}; ";
-$req_promo = "SELECT article.REDUCTION FROM ((article INNER JOIN est_en_promo USING ID_ARTICLE) INNER JOIN promotion USING ID_PROMO) where article.ID_ARTICLE=".$article_id."; ";
+//$req_promo = "SELECT article.REDUCTION FROM ((article INNER JOIN est_en_promo USING ID_ARTICLE) INNER JOIN promotion USING ID_PROMO) where article.ID_ARTICLE=".$article_id."; ";
 $req_category = "SELECT article.NOM_CATEGORIE FROM (article INNER JOIN categorie USING ID_CATEGORIE) where article.ID_ARTICLE=".$article_id."; ";
 $req_collection = "SELECT article.NOM_COLLECTION FROM (article INNER JOIN categorie USING ID_COLLECTION) where article.ID_ARTICLE=".$article_id."; ";
 $req_description = "SELECT article.DESCRIPTION FROM article where article.ID_ARTICLE=".$article_id."; ";
@@ -59,47 +61,40 @@ $req_description = "SELECT article.DESCRIPTION FROM article where article.ID_ART
             <tr>
                 <td id="tableImgProduct"><img id="imgProduct" src="img/articles/<?php echo(convertTableToString(executeSQL($req_img)));?>></td>
                 <td>
-                    <table id=" tableInfoStyle">
-            <tr>
-                <td id="infoProduct">
-                    <p> <?php echo(convertTableToString(executeSQL($req_nom_produit)));?> </p>
+                    <table id="tableInfoStyle">
+                        <tr>
+                            <td id="infoProduct">
+                                <p> <?php echo(convertTableToString(executeSQL($req_nom_produit)));?> </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td id="infoProduct">
+                                <p> <?php $prix_ttc = convertTableToString(executeSQL($req_prix_ttc));
+				                    echo($prix_ttc);?>
+                                </p>
+                            </td>
+                        </tr>
+                    
+                </td>
+                <td id="categoryProduct">
+                    <p> <?php echo(convertTableToString(executeSQL($req_category)));?> de <?php echo(convertTableToString(executeSQL($req_collection)));?> </p>
+                    
+                        <tr>
+                            <td style="padding-left:55%;">
+                                <div id="ajoutPanier">
+                                    <a href="basket.html">Ajouter au panier
+                                        <pre><br></pre></a>
+                                </div>
+                            </td>
+                        </tr>
+                  
                 </td>
             </tr>
             <tr>
-                <td id="infoProduct">
-                    <p> <?php echo(convertTableToString(executeSQL($req_prix_ht)));
-				     $prix_ttc = convertTableToString(executeSQL($req_prix_ttc));
-				     $promo = convertTableToString(executeSQL($req_promo));
-				     echo($prix_ttc);
-				     if ($promo!=NULL) {
-				         echo($promo);
-				         $ttc_promo = (int)$prix_ttc * (1- (float)$promo /100);
-				         echo($ttc_promo);
-				     } ?>
-                    </p>
+                <td colspan=3 id="descrProduct">
+                    <p> <?php echo(convertTableToString(executeSQL($req_description)));?> </p>
                 </td>
             </tr>
-        </table>
-        </td>
-        <td id="categoryProduct">
-            <p> <?php echo(convertTableToString(executeSQL($req_categorie)));?> de <?php echo(convertTableToString(executeSQL($req_collection)));?> </p>
-            <table>
-                <tr>
-                    <td style="padding-left:55%;">
-                        <div id="ajoutPanier">
-                            <a href="basket.html">Ajouter au panier
-                                <pre><br></pre></a>
-                        </div>
-                    </td>
-                </tr>
-            </table>
-        </td>
-        </tr>
-        <tr>
-            <td colspan=3 id="descrProduct">
-                <p> <?php echo(convertTableToString(executeSQL($req_description)));?> </p>
-            </td>
-        </tr>
         </table>
     </div>
 </body>
